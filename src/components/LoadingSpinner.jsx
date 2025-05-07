63feda29e0ee54aa9b863dc362a7536b35ee1a3f
Rana AbdulRehman <rana@shopxr.org>
2025-05-07 12:28:25 +0500

    Changes spinner color and speed

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -8,7 +8,7 @@ const LoadingSpinner = () => {
   return (
     <div className="absolute inset-0 flex items-center justify-center z-50 pointer-events-none">
       <div
-        className="w-16 h-16 border-4 border-gray-300 border-dotted duration-500 rounded-full border-t-gray-500"
+        className="w-16 h-16 border-4 border-gray-300 border-dotted rounded-full border-t-gray-500"
         style={{ animation: "spin 3s linear infinite" }}
       ></div>
     </div>
